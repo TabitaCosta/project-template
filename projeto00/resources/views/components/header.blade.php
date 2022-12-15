75e51318e64089c417f6f6f7afc98037bbba98e9
Tabita <tabitacostagc@gmail.com>
2022-12-14 22:19:29 -0300

    template 14/12/2022

---
+++ projeto00/resources/views/components/header.blade.php
@@ -0,0 +1,37 @@
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                <nav class="main-nav">
+                  <!-- ***** Logo Start ***** -->
+                  <a href="index.html" class="logo">
+                      <img src="assets/images/templatemo-eduwell.png" alt="EduWell Template">
+                  </a>
+                  <!-- ***** Logo End ***** -->
+                  <!-- ***** Menu Start ***** -->
+                  <ul class="nav">
+                    <li><a href="/" class="active">Home</a></li>
+                    <li><a href="/">Services</a></li>
+                    <li><a href="/">Courses</a></li>
+                    <li class="has-sub">
+                        <a href="javascript:void(0)">Pages</a>
+                        <ul class="sub-menu">
+                            <li><a href="/about">About Us</a></li>
+                            <li><a href="/services">Our Services</a></li>
+                            <li><a href="/contact">Contact Us</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="/">Testimonials</a></li> 
+                    <li><a href="/">Contact Us</a></li> 
+                  </ul>      
+                  <a class='menu-trigger'>
+                      <span>Menu</span>
+                  </a>
+                  <!-- ***** Menu End ***** -->
+                </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
